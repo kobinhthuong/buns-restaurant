03ce8a49a09fa0ccb5249d3070367fd9e81130c8
bichloannguyenkbt <bichloannguyenkbt@gmail.com>
2019-04-27 18:26:15 +0700

    cm

---
+++ front-end/layouts/footer/footer.php
@@ -0,0 +1,3 @@
+<div class="">
+    <p>Copyright &copy; 2019 Buns. All Rights Reserved | Design by Supermen </p>
+</div>
---
+++ front-end/layouts/header/header.php
@@ -0,0 +1,26 @@
+    <head>
+        <title>BUNs Admin Panel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <!-- Bootstrap Core CSS -->
+        <link href="../css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+        <!-- Custom CSS -->
+        <link href="../css/style.css" rel='stylesheet' type='text/css' />
+        <!-- Graph CSS -->
+        <link href="../css/lines.css" rel='stylesheet' type='text/css' />
+        <link href="../css/font-awesome.css" rel="stylesheet"> 
+        <!-- jQuery -->
+        <script src="../js/jquery.min.js"></script>
+        <!----webfonts--->
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+        <!---//webfonts--->  
+        <!-- Nav CSS -->
+        <link href="../css/custom.css" rel="stylesheet">
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="../js/metisMenu.min.js"></script>
+        <script src="../js/custom.js"></script>
+        <!-- Graph JavaScript -->
+        <script src="../js/d3.v3.js"></script>
+        <script src="../js/rickshaw.js"></script>
+    </head>

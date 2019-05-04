a34319e14bb67a2dd3ca58c765bd2003375df859
dieulinhng21 <dieulinhnguyen2108@gmail.com>
2019-05-04 22:31:47 +0700

    cm

--- back-end/layouts/footer/footer.php
+++ back-end/layouts/footer/footer.php
@@ -1,3 +1,3 @@
 <div class="footer-bottom" style="padding: 40px;">  
-    <p align="center" style="padding-top: 40px;"> Copyright &copy;<script>document.write(new Date().getFullYear());</script>  All Rights Reserved | Design by Supermen </p>
+    <p align="center" style="padding-top: 40px;"> Copyright &copy; <script>document.write(new Date().getFullYear());</script>  All Rights Reserved | Design by Supermen </p>
 </div>

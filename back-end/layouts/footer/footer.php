97c3f69ea5aa8950393bfd72c903a6c8d906c9f6
dieulinhng21 <dieulinhnguyen2108@gmail.com>
2019-05-04 22:28:08 +0700

    sleepy

--- back-end/layouts/footer/footer.php
+++ back-end/layouts/footer/footer.php
@@ -1,5 +1,3 @@
-<div class="footer-bottom">
-    <p  colspan="2" align="center"> Copyright &copy;<script>document.write(new Date().getFullYear());</script> All Rights Reserved | Design by Supermen </p>
-<div style="padding: 40px;">
-    <p align="center" style="padding-top: 40px;"> Copyright &copy; 2019 Buns. All Rights Reserved | Design by Supermen </p>
+<div class="footer-bottom" style="padding: 40px;">  
+    <p align="center" style="padding-top: 40px;"> Copyright &copy;<script>document.write(new Date().getFullYear());</script>  All Rights Reserved | Design by Supermen </p>
 </div>
--- upload.php
+++ upload.php
@@ -1,9 +1,9 @@
+<?php
 
-        <?php
+function upload($photo_name, $photo_tmp, $photo_path) {
+    define('SITE_ROOT', __DIR__);
+    $photo_path = SITE_ROOT . "/uploads/dishes/" . $photo_name;
+    move_uploaded_file($photo_tmp, $photo_path);
+}
 
-        function upload($photo_name, $photo_tmp, $photo_path) {
-            define('SITE_ROOT', __DIR__);
-            $photo_path = SITE_ROOT . "/uploads/dishes/" .$photo_name;
-            move_uploaded_file($photo_tmp, $photo_path);
-        }
-        ?>
+?>

ab6dfecb476cd45159c4df6b2dba7c8c6b16f9b9
phuongptmnd <phuongptmnd@gmail.com>
2019-05-06 19:38:06 +0700

    cm

--- back-end/manageDishes/add.php
+++ back-end/manageDishes/add.php
@@ -52,7 +52,6 @@ if (isset($_POST['button'])) {
                         <div>
                             <button type="submit" class="btn btn-primary" name="button">Add</button>
                         </div>
-
                     </form>
                 </div>
             </div>

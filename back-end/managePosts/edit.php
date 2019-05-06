e0d24619b2edb3a8268734586dfdd6340001f2b7
bichloannguyenkbt <bichloannguyenkbt@gmail.com>
2019-05-06 23:31:22 +0700

    cm

--- back-end/managePosts/edit.php
+++ back-end/managePosts/edit.php
@@ -1,69 +1,73 @@
 <?php
 include("../config.php");
+include("../../upload.php");
+
 $id = isset($_GET['id']) ? intval($_GET['id']) : "";
 $run = mysqli_query($con, "SELECT * FROM posts WHERE id=$id");
-$dong = mysqli_fetch_array($run);
-?>
-<?php
-if(isset($_POST['edit'])){
-    $title=$_POST['title'];
-   $summary=$_POST['summary'];
-   $content=$_POST['content'];
-   $photo=$_POST['photo'];
-    $sql="update posts set title='$title',summary='$summary',content='$content',photo='$photo' where id =$id ";
-    $run= mysqli_query($con, $sql);
-    header('location:viewall.php');
-}elseif(isset($_POST['delete'])){
-    $sql="delete from posts where id =$id ";
-    mysqli_query($con, $sql);
-    header('location:viewall.php');
+$result = mysqli_fetch_array($run);
+$error = array();
+
+if (isset($_POST['edit'])) {
+    $title = $_POST['title'];
+    $summary = $_POST['summary'];
+    $content = $_POST['content'];
+    $photo_name = $_FILES['photo']['name'];
+    $photo_tmp = $_FILES['photo']['tmp_name'];
+    $photo_path = "uploads/posts/" . $photo_name;
+
+    if (!empty($title) && !empty($summary) && !empty($content) && !empty($photo_path)) {
+        upload($photo_tmp, $photo_path);
+
+        $sql = "update posts set title='$title',summary='$summary',content='$content',photo='$photo_path' where id =$id ";
+        $run = mysqli_query($con, $sql);
+        header("Location: view.php");
+    } else {
+        array_push($error, "Dish name, dish description and photo must be filled!");
+    }
 }
 ?>
-<form method="post" enctype="multipart/form-data">
-    <table width="100%" border="1">
-        <tr>
-            <td colspan="2"><div align="center">Edit Post</div></td>
-        </tr>
-        <tr>
-            <td>Title</td>
-            <td><input type="text" name="title" value="<?php echo $dong['title']?>"></td>
-        </tr>
-        <tr>
-            <td>Summary</td>
-            <td><input type="text" name="summary" value="<?php echo $dong['summary']?>"></td>
-        </tr>
-        <tr>
-            <td>Content</td>
-            <td><textarea name="content"cols="40" rows="15"><?php echo $dong['content']?></textarea></td>
-        </tr>
-        <tr>
-            <td>Photo</td>
-            <td><input type="file" name="photo"><img src="uploads/posts/<?php echo $dong['photo']?>" width="60px" height="60px"/></td>
-        </tr>
-        <tr>
-        <td colspan="2"> <div align="center"> 
-                <button name ="edit" value="edit">edit</button>
+
+<!DOCTYPE HTML>
+<html>
+    <!--Header-->
+    <?php include ('../layouts/header/header.php'); ?>
+    <!--Navigation -->
+    <?php include ('../layouts/sidebar/navbar.php'); ?>
+    <body> 
+        <div id="page-wrapper">
+            <div class="col-md-12 graphs">
+                <div class="xs">
+                    <h3>Edit post</h3>
+                    <h5 style="color: red; te"><?php if (count($error) != 0) { echo $error[0]; }?></h5>
+                    <div class="page-header">
+                        <div class="page-name">
+                            <ol class="text-left">
+                                <a href="add.php" target="_blank">Add post</a> </br>
+                            </ol>
+                        </div>
+                    </div>
+                    <form method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" class="form-control" name="title" placeholder="<?php echo $result['title'] ?>">
+                        </div>
+                        <div class="form-group">
+                            <label>Summary</label>
+                            <input type="text" class="form-control" name="summary" placeholder="<?php echo $result['summary'];?>">
+                        </div>
+                        <div class="form-group">
+                            <label>Content</label>
+                            <textarea type="text" class="form-control" name="content" placeholder="<?php echo $result['content'];?>"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label>Photo</label>
+                            <div><img src="../../<?php echo $result['photo'];?>" style="width: 200px; height: 200px;"></div>
+                            <input type="file" class="form-control" name="photo">
+                        </div>
+                        <button type="submit" class="btn btn-primary" name="edit">Edit</button>
+                    </form>
+                </div>
             </div>
-        </td>
-    </tr>
-    </table>
-</form>    
-
-<?php
-if(isset($_POST['edit'])){
-    $title=$_POST['title'];
-   $summary=$_POST['summary'];
-   $content=$_POST['content'];
-   $photo=$_POST['photo'];
-    $sql="update posts set title='$title',summary='$summary',content='$content',photo='$photo' where id =$id ";
-    $run= mysqli_query($con, $sql);
-    header('location:view.php');
-}
-else if (isset($_POST['delete'])){
-    $sql="delete from posts where id ='$id' ";
-    mysqli_query($con, $sql);
-    header('location:view.php');
-}
-?>
-
-
+        </div>
+    </body>
+</html>

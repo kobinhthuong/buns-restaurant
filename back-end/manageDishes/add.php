d3ec1b5c7293dc2bd8c8aff7ab3f9867253f3f35
bichloannguyenkbt <bichloannguyenkbt@gmail.com>
2019-05-06 20:50:38 +0700

    fix add in dishes

--- back-end/manageDishes/add.php
+++ back-end/manageDishes/add.php
@@ -4,24 +4,23 @@ include '../../upload.php';
 
 $sql = "select * from dishes order by id desc" or die();
 $run = mysqli_query($con, $sql);
+$error = array();
 
 if (isset($_POST['button'])) {
     $dish_name = $_POST['dish_name'];
     $dish_des = $_POST['dish_des'];
     $photo_name = $_FILES['photo']['name'];
     $photo_tmp = $_FILES['photo']['tmp_name'];
-//    define('SITE_ROOT', __DIR__);
     $photo_path = "uploads/dishes/" . $photo_name;
-//    $photo_path = SITE_ROOT . "uploads/dishes/" .$photo_name;
 
-    if (isset($dish_name, $dish_des, $photo_path)) {
-//        move_uploaded_file($photo_tmp, $photo_path);
+    if (!empty($dish_name) && !empty($dish_des) && !empty($photo_path)) {
         upload($photo_tmp, $photo_path);
         $insert_dish = "INSERT INTO `dishes`(`name`, `photo`, `description`) VALUES ('$dish_name','$photo_path','$dish_des')";
-//        die('stop');
         $query = mysqli_query($con, $insert_dish);
         echo "Sucess";
         header("Location: view.php");
+    } else {
+        array_push($error, "Dish name, dish description and photo must be filled!");
     }
 }
 ?>
@@ -37,6 +36,7 @@ if (isset($_POST['button'])) {
             <div class="col-md-12 graphs">
                 <div class="xs">
                     <h3>Add new Dish</h3>
+                    <h5 style="color: red; te"><?php if (count($error) != 0) { echo $error[0]; }?></h5>
                     <form action="" method="post" enctype="multipart/form-data" >
                         <div class="form-group">
                             <input placeholder="Dish name" type="text" class="form-control" name="dish_name" >
